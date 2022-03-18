f1e9a8a192271e77f82ffa949ca6b53a502edd20
Letícia Pardini <pardini448@gmail.com>
2022-03-18 18:24:01 -0300

    alteracao codigo

--- exercicio-aula5/script.js
+++ exercicio-aula5/script.js
@@ -20,7 +20,7 @@ const CALCULADORA = (function () {
 
             for (let i = 1; i < valor.length; i++) {
                 res -= valor[i];
-                ObjCalculadora.historico.push(` O resultado da ultima subtração foi de ${this.res}`);
+                ObjCalculadora.historico.push(` O resultado da ultima subtração foi de ${res}`);
             }
             return res;
                 
@@ -31,7 +31,7 @@ const CALCULADORA = (function () {
             let res = valor[0];
             for (let i = 1; i < valor.length; i++) {
                 res = res * valor[i] ;
-                ObjCalculadora.historico.push(`O resultado da ultima multiplicação foi de ${this.res}`);
+                ObjCalculadora.historico.push(`O resultado da ultima multiplicação foi de ${res}`);
                 
 
                 return res;
@@ -40,7 +40,7 @@ const CALCULADORA = (function () {
         divisao: function (...valor) {
             let res = valor[0];
             for (const i = 1; i < valor.length; i++) {
-                res /= valor[i] ;
+                res = res / valor[i] ;
                 ObjCalculadora.historico.push(` O resultado da ultima divisao foi de ${res}`);
                 return res;
             }
@@ -80,7 +80,7 @@ console.log(CALCULADORA.soma(4, 4 ,4));
 console.log(CALCULADORA.multiplicacao(4, 4 ,4));
 console.log(CALCULADORA.subtracao(50,10,10));
 console.log(CALCULADORA.divisao(50,2,2));
-console.log(CALCULADORA.exponenciacao(3, 2));
+console.log(CALCULADORA.exponenciacao(3,2));
 console.log(CALCULADORA.historico());
 console.log(CALCULADORA.clear());
 
